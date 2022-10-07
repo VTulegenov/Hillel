2652f97f5021f26823d1ca0b1b444ec23ec800c2
PC <vladostulegenov112@gmail.com>
2022-10-07 09:23:45 +0300

    home1.1

--- homework1.py
+++ homework1.py
@@ -1,1 +1,20 @@
-print('done')
+
+a = '-'
+
+print('писать здесь:')
+
+s = input()
+
+print(50*a)
+
+print(str.upper(s[0] + s[1] + s[2] + s[3]))
+
+print(str.upper(s[-1] + s[-2] + s[-3] + s[-4]))
+
+print(50*a)
+
+
+
+
+
+
