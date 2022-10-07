6d4c0c7efbdcf7e10c744aed371526c270ced428
PC <vladostulegenov112@gmail.com>
2022-10-07 22:50:58 +0300

    home1.1

---
+++ homework1.2.py
@@ -0,0 +1,33 @@
+
+# задание №1
+a = 1
+b = 1
+c = 1
+
+print(a == b == c)
+print(a is b is c)
+print('-' * 50)
+# задание №2
+
+d = (1, 2, 3)
+f = (1, 2, 3)
+
+print(d == f)
+print(list(d) is list(f))
+print('-' * 50)
+# задание №3
+
+print(str(a) == int(b) == float(c))
+print(d is f)
+
+
+
+
+
+
+
+
+
+
+
+
--- homework1.py
+++ homework1.py
@@ -5,15 +5,13 @@ print('писать здесь:')
 
 s = input()
 
+t = str.upper(s[0:4])
+y = str.upper(s[:-5:-1])
+
 print(50*a)
-
-print(str.upper(s[0:4]))
-
-print((str.upper(s[:-5:-1])))
-
-print(a*50)
-
-
+print(t)
+print(y)
+print(50*a)
 
 
 
