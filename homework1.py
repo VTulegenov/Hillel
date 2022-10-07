3ff2e54aa3d7af2efbc45ee56268699ae25675dc
PC <vladostulegenov112@gmail.com>
2022-10-07 20:02:40 +0300

    home1.2

--- homework1.py
+++ homework1.py
@@ -7,11 +7,16 @@ s = input()
 
 print(50*a)
 
-print(str.upper(s[0] + s[1] + s[2] + s[3]))
+print(str.upper(s[0:4]))
 
-print(str.upper(s[-1] + s[-2] + s[-3] + s[-4]))
+print((str.upper(s[:-5:-1])))
 
-print(50*a)
+print(a*50)
+
+
+
+
+
 
 
 
