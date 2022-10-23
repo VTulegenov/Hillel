1ccffdcbb519d92add461b157becd19368e21a60
PC <vladostulegenov112@gmail.com>
2022-10-23 11:59:52 +0300

    home1

---
+++ home1.py
@@ -0,0 +1,24 @@
+# задание №1
+a = 1, 2
+b = 1, 2
+c = 1, 2
+
+print(a == b == c)
+print(a is b is c)
+print('-' * 50)
+# задание №2
+
+d = (1, 2, 3)
+f = (1, 2, 3)
+
+print(d == f)
+print(list(d) is list(f))
+print('-' * 50)
+# задание №3
+print(list(a) == list(b) == list(c))
+print(str(a) is str(b) is str(c))
+print(d is f)
+print(d == f)
+
+
+
