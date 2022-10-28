783d05190efd6f938985756236d33ec91eab87f0
PC <vladostulegenov112@gmail.com>
2022-10-28 09:01:39 +0300

    home11

--- home1.1.py
+++ home1.1.py
@@ -1,4 +1,5 @@
 dict_old = {1: 9, 2: 8, 3: 7, 4: 6, 5: 5, 6: 4, 7: 3, 8: 2, 9: 1}
 print(dict_old)
-dict_new = {v: k for k, v in dict_old.items()}
+dict_new = {g: h for h, g in dict_old.items()}
 print(dict_new)
+
