3d528edae664794976d53a386643362111e365df
PC <vladostulegenov112@gmail.com>
2022-11-04 14:59:59 +0200

    home17

--- homework14.py
+++ homework14.py
@@ -1,6 +1,7 @@
 
 
 while True:
+    print('если хотите выйти введите:ВЫХОД, EXIT, QUIT, E, Q')
     value = input('Ведите число:')
 
     if value.upper() in ("ВЫХОД", "EXIT", "QUIT", "E", "Q"):
@@ -9,19 +10,19 @@ while True:
     elif value.isdigit():
         print('Вы ввели число: ' + value)
 
-    elif '-' in value and not '.' in value:
+    elif '-' in value and not '.' in value and value.isdigit():
         print('Вы ввели отрицательное число: ' + value)
 
-    elif '-.' in value:
+    elif '-.' in value and value.isdigit():
         print('Вы ввели отрицательное дробное число: ' + value[:1], '0', value[1:], sep='')
 
-    elif '-' in value and '.' in value:
+    elif '-' in value and '.' in value and value.isdigit():
         print('Вы ввели отрицательное дробное число: ' + value)
 
-    elif '.' in value:
+    elif '.' in value and value.isdigit():
         print('Вы ввели дробное число: ' + value)
 
-    elif not value.isdigit():
+    else:
         print("Вы ввели не корректное число: " + value)
 
 
