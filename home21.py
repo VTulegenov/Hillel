5ab8fd58699c4c19ea026c33da9d655dd986cabc
PC <vladostulegenov112@gmail.com>
2022-11-11 18:43:02 +0200

    home21_2-0

--- home21.py
+++ home21.py
@@ -14,15 +14,16 @@ class auto:
 
     def move(self):
         print('move')
-    def birthday(self):
-        self.age += 1
-        print(self.age)
+    def birthday(self, age):
+        self.age = age
+        age += 1
+        print(age)
     def stop(self):
         print('stop')
 
 auto_1 = auto('brand', 'age', 'mark')
 auto_1.move()
-auto_1.birthday()
+auto_1.birthday(1)
 auto_1.stop()
-auto_1.birthday()
-auto_1.birthday()
+auto_1.birthday(1)
+auto_1.birthday(1)
