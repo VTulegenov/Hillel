42822e1a8570576ec21b47d0e400200ea009bcf6
PC <vladostulegenov112@gmail.com>
2022-12-01 17:01:30 +0200

    home27

---
+++ home27.py
@@ -0,0 +1,36 @@
+class Negative_degree(Exception):
+    def __init__(self, message='Возведение в отрицательную степень'):
+        super().__init__(message)
+
+
+class Calculator(int):
+
+    def __init__(self, var):
+        self.var = var
+
+    def __add__(self, other):
+        try:
+            return Calculator(self.var + other)
+        except TypeError:
+            return 'Нельзя складывать цифры и буквы!'
+
+    def __sub__(self, other):
+        try:
+            return Calculator(self.var - other)
+        except TypeError:
+            return 'Нельзя от цифры отнять буквы!'
+
+    def __pow__(self, power, modulo=None):
+        if power >= 0:
+            return Calculator(self.var ** power)
+        else:
+            raise Negative_degree()
+
+    def __truediv__(self, other):
+        try:
+            return Calculator(self.var / other)
+        except ZeroDivisionError:
+            return 'Нельзя делить на ноль!'
+        except TypeError:
+            return 'Нельзя делить на буквы!'
+
